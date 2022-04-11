ee6812a794e027b1144085111c4c64119b1a3a69
Asahi-cloud-lang <hiroto_asahi@yahoo.co.jp>
2022-04-11 13:28:39 +0000

    Update Gemfile for Heroku

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -7,17 +7,35 @@ class LinebotController < ApplicationController
 
     def push
         @task = Task.find(params[:task_id])
-        message = {
-            type: 'text',
-            text: @task.name
+        # message = {
+        #     type: 'text',
+        #     text: @task.name
+        # }
+        # client ||= Line::Bot::Client.new { |config|
+        #     config.channel_id = ENV['CHANNEL_ID']
+        #     config.channel_secret = ENV['CHANNEL_SECRET']
+        #     config.channel_token = ENV['CHANNEL_TOKEN']
+        # }
+        # user_id =  ENV['USER_ID']
+        # response = client.push_message(user_id, message)
+        
+        # post先のurl
+        uri = URI.parse('https://api.line.me/v2/bot/message/broadcast')
+        http = Net::HTTP.new(uri.host,uri.port)
+        http.use_ssl = true
+
+        token = ENV['CHANNEL_TOKEN']
+        # Header
+        headers = {
+            'Authorization'=>"Bearer #{token}",
+            'Content-Type' =>'application/json',
+            'Accept'=>'application/json'
         }
-        client ||= Line::Bot::Client.new { |config|
-            config.channel_id = ENV['CHANNEL_ID']
-            config.channel_secret = ENV['CHANNEL_SECRET']
-            config.channel_token = ENV['CHANNEL_TOKEN']
-        }
-        user_id =  ENV['USER_ID']
-        client.push_message(user_id, message)
+        send_message = @task.name
+        # Body
+        params = {"messages" => [{"type" => "text", "text" => send_message}]}
+
+        response = http.post(uri.path, params.to_json, headers)
     end
 end
 
