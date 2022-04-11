23ff312f458fa1dd31d78b5d285781560f02fdbe
Asahi-cloud-lang <hiroto_asahi@yahoo.co.jp>
2022-04-11 14:13:37 +0000

    Update Gemfile for Heroku

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,44 +1,27 @@
 class LinebotController < ApplicationController
   require 'line/bot'
+  require 'httpclient'
+  require 'net/http'
   
     def index
         render status: 200 , json: { status: 200, message: "Success" }
     end
 
     def push
-        require 'net/http'
-        require 'uri'
-        require 'json' 
         @task = Task.find(params[:task_id])
-        # message = {
-        #     type: 'text',
-        #     text: @task.name
-        # }
-        # client ||= Line::Bot::Client.new { |config|
-        #     config.channel_id = ENV['CHANNEL_ID']
-        #     config.channel_secret = ENV['CHANNEL_SECRET']
-        #     config.channel_token = ENV['CHANNEL_TOKEN']
-        # }
-        # user_id =  ENV['USER_ID']
-        # response = client.push_message(user_id, message)
-        
-        # post先のurl
-        uri = URI.parse('https://api.line.me/v2/bot/message/broadcast')
-        http = Net::HTTP.new(uri.host,uri.port)
-        http.use_ssl = true
-
-        token = ENV['CHANNEL_TOKEN']
-        # Header
-        headers = {
-            'Authorization'=>"Bearer #{token}",
-            'Content-Type' =>'application/json',
-            'Accept'=>'application/json'
+        message = {
+            type: 'text',
+            text: @task.name
         }
-        send_message = @task.name
-        # Body
-        params = {"messages" => [{"type" => "text", "text" => send_message}]}
-
-        response = http.post(uri.path, params.to_json, headers)
+        client ||= Line::Bot::Client.new { |config|
+            config.channel_id = ENV['CHANNEL_ID']
+            config.channel_secret = ENV['CHANNEL_SECRET']
+            config.channel_token = ENV['CHANNEL_TOKEN']
+        }
+        user_id =  ENV['USER_ID']
+        response = client.push_message(user_id, message)
+        
+        
     end
 end
 
