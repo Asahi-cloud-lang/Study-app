440d03f051075d3a39740949997b1a31d491e1ef
Asahi-cloud-lang <hiroto_asahi@yahoo.co.jp>
2022-04-11 12:42:19 +0000

    Update Gemfile for Heroku

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -6,24 +6,19 @@ class LinebotController < ApplicationController
     end
 
     def push
-         @task = Task.find(params[:task_id])
+        @task = Task.find(params[:task_id])
+        client ||= Line::Bot::Client.new { |config|
+            config.channel_id = ENV['CHANNEL_ID']
+            config.channel_secret = ENV['CHANNEL_SECRET']
+            config.channel_token = ENV['CHANNEL_TOKEN']
+        }
         message={
             type: 'text',
             text: @task.name
            }
         user_id =  ENV['USER_ID']
-        response = client.push_message(user_id, message)
-    end
-
-    private
-
-    # LINE Developers登録完了後に作成される環境変数の認証
-    def client
-        @client ||= Line::Bot::Client.new { |config|
-        config.channel_id = ENV['CHANNEL_ID']
-        config.channel_secret = ENV['CHANNEL_SECRET']
-        config.channel_token = ENV['CHANNEL_TOKEN']
-        }
+        response = client.broadcast(message)
+        p response
     end
 end
 
