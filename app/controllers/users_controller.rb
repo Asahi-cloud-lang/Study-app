fc9aa6d953b590d99ce7354a1f0fa395aeb5b1a8
Asahi-cloud-lang <hiroto_asahi@yahoo.co.jp>
2022-04-13 10:12:24 +0000

    Update Gemfile for Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -163,7 +163,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
     def basic_info_params
